6fed490a9e959ab3e13a8509f07e20ca755b3894
Pavel Timofeev <timos500@mail.ru>
2020-07-21 14:19:53 +0300

    basic communication

--- src/pal/main.c
+++ src/pal/main.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <string.h>
 
 #include "pa1_starter_code/pa1.h"
 #include "pa1_starter_code/ipc.h"
 
 void createProcesses(int count);
+
 void run(local_id id);
+
 void waitProcesses(int count);
+
+int fd[2];
 
 
 int main(int argc, char *argv[]) {
@@ -19,11 +25,23 @@ int main(int argc, char *argv[]) {
      * 3. Мониторинг работы дочерних процессов
      */
 
+
+    Message message;
+    pipe(fd);
+
     int processesCount = (int) strtol(argv[2], NULL, 10);
 
     createProcesses(processesCount);
     waitProcesses(processesCount);
 
+
+    for (int i = 0; i <processesCount; ++i) {
+        read(fd[0], &message, MAX_MESSAGE_LEN);
+        printf("%s", message.s_payload);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
 
     return 0;
 }
@@ -31,21 +49,40 @@ int main(int argc, char *argv[]) {
 
 void createProcesses(int count) {
     for (int i = 1; i < count + 1; ++i) {
-        if (fork() == 0) {
+        if (fork() == 0) {  // если это дочерний процесс, то ...
             run(i);
         }
     }
 }
 
 void run(local_id id) {
-    printf(log_started_fmt, id, getpid(), getppid());
+
+    char createLog[MAX_PAYLOAD_LEN];
+
+    sprintf(createLog, log_started_fmt, id, getpid(), getppid());
+    printf("%s", createLog);
+
+    Message message;
+
+    message.s_header.s_magic = MESSAGE_MAGIC;
+    message.s_header.s_type = STARTED;
+    message.s_header.s_local_time = time(NULL);
+    message.s_header.s_payload_len = MAX_PAYLOAD_LEN;
+
+    strcpy(message.s_payload, createLog);
+
+    write(fd[1], &message, MAX_MESSAGE_LEN);
+
+    close(fd[0]);
+    close(fd[1]);
+
     exit(0);
 }
 
 void waitProcesses(int count) {
     for (int i = 0; i < count; i++) {
         int pid = wait(NULL);
-        printf("child process with pid = %d was finished\n", pid);
+        //printf("child process with pid = %d was finished\n", pid);
     }
 }
 
