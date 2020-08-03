863d650bef18d5c5ae1a69003d1f288add481c2a
Pavel Timofeev <timos500@mail.ru>
2020-08-04 00:48:04 +0300

    passed :)

--- src/pa1/pipes.c
+++ src/pa1/pipes.c
@@ -30,11 +30,21 @@ void closePipes(ProcessPipes *processPipes, int procCount, local_id id) {
 void closeOtherParentDescriptors(ProcessPipes *processPipes, int procCount) {
     for (int i = 0; i < procCount; ++i) {
         for (int j = 0; j < procCount; ++j) {
-            if (i != j && j != PARENT_ID) {
-                close(processPipes->pipes[i][j][READ_DESC]);
-                close(processPipes->pipes[i][j][WRITE_DESC]);
-                logCloseDescriptor(processPipes->pipes[i][j][READ_DESC], PARENT_ID);
-                logCloseDescriptor(processPipes->pipes[i][j][WRITE_DESC], PARENT_ID);
+            if (i != j){
+                if (j != PARENT_ID && i != PARENT_ID) {
+                    close(processPipes->pipes[i][j][READ_DESC]);
+                    close(processPipes->pipes[i][j][WRITE_DESC]);
+                    logCloseDescriptor(processPipes->pipes[i][j][READ_DESC], PARENT_ID);
+                    logCloseDescriptor(processPipes->pipes[i][j][WRITE_DESC], PARENT_ID);
+                } else {
+                    if (j == PARENT_ID) {
+                        close(processPipes->pipes[i][j][WRITE_DESC]);
+                        logCloseDescriptor(processPipes->pipes[i][j][WRITE_DESC], PARENT_ID);
+                    } else {
+                        close(processPipes->pipes[i][j][READ_DESC]);
+                        logCloseDescriptor(processPipes->pipes[i][j][READ_DESC], PARENT_ID);
+                    }
+                }
             }
         }
     }
@@ -48,7 +58,7 @@ void closeOtherChildDescriptors(ProcessPipes *processPipes, local_id id, int pro
                     close(processPipes->pipes[i][j][READ_DESC]);
                     logCloseDescriptor(processPipes->pipes[i][j][READ_DESC], id);
                 } else {
-                    if (j == id && i > 0) {
+                    if (j == id) {
                         close(processPipes->pipes[i][j][WRITE_DESC]);
                         logCloseDescriptor(processPipes->pipes[i][j][WRITE_DESC], id);
                     } else {
